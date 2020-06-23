9d3630a0f0b5c102b8cf8f09409ba171de5005ab
RobinLoksli <ami0504@mail.ru>
2020-06-23 19:13:46 +1000

    newWhere

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -18,25 +18,44 @@ exports.actionIndex = async (req, res) => {
     //     sql: true,
     // });
 
-    let user = {
-        firstname : 'Мой сосед',
-        lastname  : 'Мудак',
-        // email     : '@lol.ru',
-        pass      : '123',
-        group_id  : '1',
-        role_id   : 1,
-        smth      : {kek: 'lol'},
-        azaza     : false,
-        date      : new Date(),
-        phone     : '89241098357',
-    }
+    // let user = {
+    //     firstname : 'Мой сосед',
+    //     lastname  : 'Мудак',
+    //     // email     : '@lol.ru',
+    //     pass      : '123',
+    //     group_id  : '1',
+    //     role_id   : 1,
+    //     smth      : {kek: 'lol'},
+    //     azaza     : false,
+    //     date      : DateModule.formatDbDate(new Date()),
+    //     phone     : '89241098357',
+    // }
 
-    d = new Date();
-    d = DateModule.formatDbDateTime(d);
+    // d = new Date();
+    // d = DateModule.formatDbDateTime(d);
+    //
+    // let result = await User.save(user);
+    // console.log(result);
 
-    let result = await User.save(user);
+    let user = await User.find('all', {
+        // newWhere: [
+        //     "(id < $id) AND timetest = $time",
+        //     {
+        //         id  : 5,
+        //         time: DateModule.formatDbTime(new Date()),
+        //     }
+        // ],
+        // newWhere: [
+        //     'id = $id', {id: 5},
+        // ],
+        // newWhere: [
+        //     'id IN ($array)', {array: [1, 3, 5]},
+        // ],
+        // newWhere: ['email LIKE $val', {val: '%kek%'}],
+        newWhere: ['id BETWEEN $val1 AND $val2', {val1: 1, val2: 5}],
+        // sql: true,
+    });
 
-    console.log(result);
     res.send(user);
     // res.render('index', {user: user});
 
