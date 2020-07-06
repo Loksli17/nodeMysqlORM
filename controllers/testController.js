c034a8c79b67b0306ad8aeaebd321d5caebccebb
BlackLirium <mrfilin97@gmail.com>
2020-07-06 19:00:07 +1000

    newConnetion

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,26 +1,12 @@
 const crypto = require('crypto');
 
-const DateModule = require('./../lib/date');
-const UserModel = require('./../models/UserModel');
-const User = new UserModel();
+let dataBaseConnection =  require('./../lib/database/newDatabase');
+connetion = new dataBaseConnection();
+connetion.disconection();
+
 
 exports.actionIndex = async (req, res) => {
-    let users = [];
-    let fuck = req.query.id;
-     
-    console.log(req.query.id);
-    if (req.query.id != undefined){
-        users = await User.find ('all', {
-            newWhere: [
-                'id = $id',
-                 {id : req.query.id}
-            ],
-        });
-        res.send(users);
-        return;
-    }else{
-        users = await User.find('all');
-        res.send(users);
-        return;
-    }
+
 }
+
+// console.log(connetion.disconection());
